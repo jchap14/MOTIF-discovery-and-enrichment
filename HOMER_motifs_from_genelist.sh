e485370b8fff242fa829037850a106328bc5ef2c
James <jchap14@gmail.com>
2018-03-13 23:01:24 -0700

    d

--- HOMER_motifs_from_genelist.sh
+++ HOMER_motifs_from_genelist.sh
@@ -6,6 +6,6 @@
 # find *.genes | xargs -n1 bash HOMER_motifs_from_genelist.sh #local, analyze all
 
 name=`basename $1 .genes`
-findMotifs.pl $1 human $name.HOMERmotif_500  -start -400  -end 100 -p 4 -dumpFasta
-findMotifs.pl $1 human $name.HOMERmotif_2500 -start -2000 -end 500 -p 4 -dumpFasta
+findMotifs.pl $1 human $name.HOMERmotif_500  -start -50  -end 50 -p 4 -dumpFasta
+#findMotifs.pl $1 human $name.HOMERmotif_2500 -start -2000 -end 500 -p 4 -dumpFasta
 rm motifFindingParameters.txt
