3b536983f50d56213c513309ea06fdea837e4859
James <jchap14@gmail.com>
2018-03-14 18:45:24 -0700

    make qsub-able

--- HOMER_motifs_from_genelist.sh
+++ HOMER_motifs_from_genelist.sh
@@ -1,11 +1,38 @@
+#!/bin/bash
+
 ################### HOMER (findMotifs.pl): Find MOTIFs in promoters with gene list input
 ### Prefers refseq IDs if possible, but can convert SYMBOLs
 ## http://homer.salk.edu/homer/motif/
 
-#!/bin/bash
-# find *.genes | xargs -n1 bash HOMER_motifs_from_genelist.sh #local, analyze all
+## run on SCG
+## find *.genes | xargs -n1 bash HOMER_motifs_from_genelist.sh
 
-name=`basename $1 .genes`
-findMotifs.pl $1 human $name.HOMERmotif_500  -start -50  -end 50 -p 4 -dumpFasta
+## modules: homer & weblogo are loaded in conda environment
+
+## Variables:
+NAME=`basename $1 .genes`
+
+cat > $NAME.tempscript.sh << EOF
+#!/bin/bash
+#$ -N $NAME.motifsFromBed
+#$ -j y
+#$ -cwd
+#$ -V
+#$ -l h_vmem=1G
+#$ -pe shm 12
+#$ -l h_rt=5:59:00
+#$ -l s_rt=5:59:00
+
+##### Run commands:
+## add -dumpFasta to get FASTA output also
+findMotifs.pl $1 human $NAME.HOMERmotif_500  -start -50  -end 50 -p 4 -dumpFasta
 #findMotifs.pl $1 human $name.HOMERmotif_2500 -start -2000 -end 500 -p 4 -dumpFasta
 rm motifFindingParameters.txt
+
+EOF
+
+## qsub then remove the tempscript
+qsub $NAME.tempscript.sh
+sleep 1
+rm $NAME.tempscript.sh
+
